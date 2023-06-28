5fe49df744679a54d385f7cc541843f4e51c172d
Mark Manfrey <mark@usehyphen.com>
2023-06-28 12:49:29 -0700

    second commit working POST create

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const routes = require('./routes/html'); // import the routes
+const bodyParser = require("body-parser");
+const app = express();
+const helmet = require('helmet');
+const compression = require('compression');
+
+app.use(express.urlencoded({
+    extended:true
+}));
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+    //res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Content-Type', 'multipart/form-data')
+    res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.use('/', routes); //to use the routes
+
+app.use(helmet());
+app.use(compression());
+
+
+
+const listener = app.listen(process.env.PORT || PORT, () => {
+    console.log('App is listening on port ' + listener.address().port)
+})
+
+const connectDB = async () => {
+    try {
+        //mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGODB_URI,{ family: 4});
+        console.log(`MongoDB Connected: {conn.connection.host}`);
+        console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+  }
+  connectDB();
+
