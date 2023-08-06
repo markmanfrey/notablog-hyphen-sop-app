86f066602b4eb52b3ee883773981772f0417415a
Mark Manfrey <mark@usehyphen.com>
2023-08-05 19:22:46 -0700

    blah

--- notablog-app/dist/config.d.ts
+++ notablog-app/dist/config.d.ts
@@ -1,10 +1,11 @@
-import { noteablog-appStarterConfig } from './types';
+import { noteablogappStarterConfig } from './types';
+
 export declare class Config {
     private configPath;
     private configObj;
     constructor(configPath: string);
-    get<T extends keyof noteablog-appStarterConfig>(key: T): noteablog-appStarterConfig[T];
-    set<T extends keyof noteablog-appStarterConfig>(key: T, data: noteablog-appStarterConfig[T]): void;
+    get<T extends keyof noteablogappStarterConfig>(key: T): noteablogappStarterConfig[T];
+    set<T extends keyof noteablogappStarterConfig>(key: T, data: noteablogappStarterConfig[T]): void;
     /** Sync changes to file. */
     sync(): Promise<void>;
 }
